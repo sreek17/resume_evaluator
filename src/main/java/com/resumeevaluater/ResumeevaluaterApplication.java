a4ae8b3ba60bb0642d3f9bfa2b7acd626cfb7bcf
Sreekar Pappu <sreekarpappu@Sreekars-MacBook-Pro.local>
2023-03-11 15:10:55 -0500

    initial commit

---
+++ src/main/java/com/resumeevaluater/ResumeevaluaterApplication.java
@@ -0,0 +1,13 @@
+package com.resumeevaluater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeevaluaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeevaluaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resumeevaluater/ResumeevaluaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeevaluater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeevaluaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
